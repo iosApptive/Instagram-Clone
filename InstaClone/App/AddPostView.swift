147100908e9644b91db5600aa0a88406adca2232
guyvenson cadet <createwidthcodes@gmail.com>
2025-09-07 12:48:33 -0400

    setup tabview

---
+++ InstaClone/App/AddPostView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPostView.swift
+//  InstaClone
+//
+//  Created by Guyvenson Cadet on 9/7/25.
+//
+
+import SwiftUI
+
+struct AddPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPostView()
+}
---
+++ InstaClone/App/ReelsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReelsView.swift
+//  InstaClone
+//
+//  Created by Guyvenson Cadet on 9/7/25.
+//
+
+import SwiftUI
+
+struct ReelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelsView()
+}
