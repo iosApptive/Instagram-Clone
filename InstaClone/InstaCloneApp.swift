35f980d03235af8fc48a943183ddbcb69de7eccb
guyvenson cadet <createwidthcodes@gmail.com>
2025-09-07 12:38:42 -0400

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Guyvenson Cadet on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaCloneTests/InstaCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneTests.swift
+//  InstaCloneTests
+//
+//  Created by Guyvenson Cadet on 9/7/25.
+//
+
+import Testing
+@testable import InstaClone
+
+struct InstaCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
